8c7769b012fceb1936c16d998e8f6467c568406c
Syahid <earlsyahid@gmail.com>
2025-09-18 20:11:12 +0700

    Demokonversisuhu.java

--- Demokonversisuhu.java
+++ Demokonversisuhu.java
@@ -5,10 +5,11 @@ public class Demokonversisuhu {
         System.out.println("--------------------KonversiSuhu--------------------");
         System.out.println("====================================================");
         System.out.println("Celcius = 20");
-        System.out.println("Celcius = 40");
+        System.out.println("Fahrenheit = 40");
         System.out.println("=====================================================");
         System.out.println("Celciustofahrenheit =" + suhu.celciustofahrenheit(20));
         System.out.println("Celciustoreamur =" + suhu.celciustoreamur(20));
         System.out.println("Fahrenheittoreamur =" + suhu.fahrenheittoreamur(40));
     }
 }
+
